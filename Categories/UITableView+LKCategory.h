6491fc63cd3db903745bf7de142855082bfbdeb1
Scott Little <scott@littleknownsoftware.com>
2010-03-09 12:36:06 +0100

    Added new category on the UITableView to get row position

---
+++ Categories/UITableView+LKCategory.h
@@ -0,0 +1,24 @@
+//
+//  UITableView+LKCategory.h
+//  Cavity
+//
+//  Created by Scott Little on 09/03/10.
+//  Copyright 2010 Little Known Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+typedef enum {
+	kLKCellGroupingSingle,
+	kLKCellGroupingFirst,
+	kLKCellGroupingMiddle,
+	kLKCellGroupingLast,
+} LKTableViewCellGroupingType;
+
+
+@interface UITableView (LKCategory)
+
+- (LKTableViewCellGroupingType)cellGroupingForCellAtIndexPath:(NSIndexPath *)indexPath;
+
+@end
---
+++ Categories/UITableView+LKCategory.m
@@ -0,0 +1,37 @@
+//
+//  UITableView+LKCategory.m
+//  Cavity
+//
+//  Created by Scott Little on 09/03/10.
+//  Copyright 2010 Little Known Software. All rights reserved.
+//
+
+#import "UITableView+LKCategory.h"
+
+
+@implementation UITableView (LKCategory)
+
+- (LKTableViewCellGroupingType)cellGroupingForCellAtIndexPath:(NSIndexPath *)indexPath {
+	
+	LKTableViewCellGroupingType	cellGroupingType = kLKCellGroupingSingle;
+	NSUInteger					sectionRows = [self numberOfRowsInSection:indexPath.section];
+	
+	//	If it is not a single cell...
+	if (sectionRows > 1) {
+		//	Can't use a switch to test for the last value so just use ifs :-(
+		if (indexPath.row == (sectionRows - 1)) {
+			cellGroupingType = kLKCellGroupingLast;
+		}
+		else if (indexPath.row == 0) {
+			cellGroupingType = kLKCellGroupingFirst;
+		}
+		else {
+			cellGroupingType = kLKCellGroupingMiddle;
+		}
+	}
+	
+	return cellGroupingType;
+}
+
+
+@end
