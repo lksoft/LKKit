d52e52c2ac3a6f0fcafe8b3511d77542de59d198
Scott Little <scott@littleknownsoftware.com>
2010-03-24 00:49:49 +0100

    Added rect convenience functions

--- LKCGDrawing.h
+++ LKCGDrawing.h
@@ -9,3 +9,15 @@
 #import <UIKit/UIKit.h>
 
 CGMutablePathRef	LKCreateRoundedRectPath(CGRect desiredRect, CGFloat cornerRadius, CGFloat lineWidth);
+
+CGRect	LKRectBySettingX(CGRect originalRect, CGFloat newX);
+CGRect	LKRectBySettingY(CGRect originalRect, CGFloat newY);
+CGRect	LKRectBySettingWidth(CGRect originalRect, CGFloat newWidth);
+CGRect	LKRectBySettingHeight(CGRect originalRect, CGFloat newHeight);
+CGRect	LKRectBySettingOrigin(CGRect originalRect, CGPoint newOrigin);
+CGRect	LKRectBySettingSize(CGRect originalRect, CGSize newSize);
+
+CGRect	LKRectByOffsettingX(CGRect originalRect, CGFloat xOffset);
+CGRect	LKRectByOffsettingY(CGRect originalRect, CGFloat yOffset);
+CGRect	LKRectByAdjustingWidth(CGRect originalRect, CGFloat width);
+CGRect	LKRectByAdjustingHeight(CGRect originalRect, CGFloat height);
