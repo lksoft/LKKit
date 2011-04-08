b6c11f10630372f13271c05933120898701b6d6a
Scott Little <scott@littleknownsoftware.com>
2011-04-08 18:10:44 +0200

    Create project and structure for the kit.

---
+++ KitTests/KitTests.h
@@ -0,0 +1,17 @@
+//
+//  KitTests.h
+//  KitTests
+//
+//  Created by Scott Little on 08/04/2011.
+//  Copyright 2011 Little Known Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface KitTests : SenTestCase {
+@private
+    
+}
+
+@end
